6b5853713dfe3b940b91ade447c88fc98b77b4f1
Vroom <harrison.hilpert@gmail.com>
2023-08-12 20:06:31 -0700

    Full reconfig of adding and subtracting

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-# Useful datatypes
+# Useful datatype for two inputs from the user
 class dualInputs:
     def __init__(self) -> None:
         self.inputOne = input("Parameter one: ")
         self.inputTwo = input("Parameter two: ")
+
+# Function to reduce reused code
+def addSub(modifier):
+    usrInput = dualInputs()
+    result = int(usrInput.inputOne) + (int(usrInput.inputTwo) * modifier)
+    print(f"The result is: {result}")
 
 # Intro sequence
 print(""""Welcome to the god awful calculator!
@@ -13,15 +19,14 @@ while True:
     # Baseline command
     usrCommand = input("-> ")
     
+    # Switch statement to choose operation mode
     match usrCommand.lower():
         #Handle Addition situation
-        case "addition":
-            usrInput = dualInputs()
-            result = int(usrInput.inputOne) + int(usrInput.inputTwo)
-            print(f"The result is: {result}")
+        case "add":
+            addSub(1)
         
         case "subtract":
-            continue
+            addSub(-1)
 
         # Command to exit the program
         case "exit": break
