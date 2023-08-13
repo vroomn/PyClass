b56852b1c4bd1b62f7d27eff61e5de6e66ba20f7
Vroom <harrison.hilpert@gmail.com>
2023-08-12 19:26:07 -0700

    Baseline execution

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 # Intro sequence
-print("Welcome to the god awful calculator")
+print(""""Welcome to the god awful calculator!
+All operations are availible under \"help\"""")
+
+# Primary execution process
+execute = True
+while True:
+    # Baseline command
+    usrCommand = input("-> ")
+    
+    match usrCommand.lower():
+        # Command to exit the program
+        case "exit":
+            execute = False
+            break
